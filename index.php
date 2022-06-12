2a575dc662d579c1e187095f542683253186cdce
HarisNestumm <104586014+HarisNestumm@users.noreply.github.com>
2022-06-13 00:31:20 +0800

    Update index.php

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
 		$uri = 'http://';
 	}
 	$uri .= $_SERVER['HTTP_HOST'];
-	header('Location: '.$uri.'/smappersonal.php');
+	header('Location: '.$uri.'/smappersonal.html');
 	exit;
 ?>
 Something is wrong with the XAMPP installation :-(
