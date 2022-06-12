27c7c7b0a6e140b0c01648117640cea8970652dd
HarisNestumm <104586014+HarisNestumm@users.noreply.github.com>
2022-06-13 00:34:23 +0800

    Initialize first page

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
 		$uri = 'http://';
 	}
 	$uri .= $_SERVER['HTTP_HOST'];
-	header('Location: '.$uri.'/smappersonal.html');
+	header('Location: '.$uri.'/login.php');
 	exit;
 ?>
 Something is wrong with the XAMPP installation :-(
